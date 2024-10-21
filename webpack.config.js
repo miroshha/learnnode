92976fbefb5c85e4d09d87803b31ffe96984c1a2
miroshha <miron.gavva@tptlive.ee>
2024-10-21 10:21:03 +0300

    add html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 import path from 'path'
-const __filename = import.meta.filename
-const __dirname = import.meta.dirname
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 export default {
     entry: './src/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin(),
+    ],
 }
