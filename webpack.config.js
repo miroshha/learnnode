96b289c0f82c1912948fa8bae1efcbb453b27bb0
miroshha <miron.gavva@tptlive.ee>
2024-11-11 11:39:59 +0200

    rick and morty api characters

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,9 @@ import HtmlWebpackPlugin from 'html-webpack-plugin'
 const __dirname = import.meta.dirname
 const name = 'Miron'
 
+const response = await fetch('https://rickandmortyapi.com/api/character');
+const data = await response.json()
+const characters = data.results
 export default {
     entry: './src/index.js',
     output: {
@@ -54,6 +57,7 @@ export default {
             template: './src/index.njk',
             templateParameters: {
                 name: name,
+                characters: characters
             }
         }),
         new HtmlWebpackPlugin({
