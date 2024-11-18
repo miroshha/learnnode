a455c18fea18cf06f4c591a297f20d403328324f
miroshha <miron.gavva@tptlive.ee>
2024-11-18 11:47:22 +0200

    images and tables

--- webpack.config.js
+++ webpack.config.js
@@ -1,68 +1,81 @@
 import path from 'path'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
-
 const __dirname = import.meta.dirname
 const name = 'Miron'
 
-const response = await fetch('https://rickandmortyapi.com/api/character');
-const data = await response.json()
-const characters = data.results
-export default {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'public')
+export default async () => {
+    const response = await fetch('https://rickandmortyapi.com/api/character');
+    const data = await response.json();
+    const characters = data.results;
+    const pages = [];
+    characters.forEach(character => {
+        let page = new HtmlWebpackPlugin({
+            template: './src/character.njk',
+            filename: `character_${character.id}.html`,
+            templateParameters: {
+                character
+            }
+        });
+        pages.push(page);
+    });
+    return {
+        entry: './src/index.js',
+        output: {
+            filename: 'main.js',
+            path: path.resolve(__dirname, 'dist')
         },
-        compress: true,
-        port: 9000,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
+        devServer: {
+            static: {
+                directory: path.join(__dirname, 'public')
             },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    "style-loader",
-                    "css-loader",
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            sassOptions: {
-                                quietDeps: true,
+            compress: true,
+            port: 9000,
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.css$/i,
+                    use: ["style-loader", "css-loader"],
+                },
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                        "style-loader",
+                        "css-loader",
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sassOptions: {
+                                    quietDeps: true,
+                                }
                             }
                         }
-                    }
-                ],
-            },
-            {
-                test: /\.njk$/,
-                use: [
-                    {
-                        loader: 'simple-nunjucks-loader',
-                        options: {}
-                    }
-                ]
-            }
+                    ],
+                },
+                {
+                    test: /\.njk$/,
+                    use: [
+                        {
+                            loader: 'simple-nunjucks-loader',
+                            options: {}
+                        }
+                    ]
+                }
+            ],
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './src/index.njk',
+                templateParameters: {
+                    name: name,
+                    characters: characters
+                }
+            }),
+            new HtmlWebpackPlugin({
+                template: './src/about.njk',
+                filename: 'about.html'
+            }),
+            ...pages
         ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.njk',
-            templateParameters: {
-                name: name,
-                characters: characters
-            }
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/about.njk',
-            filename: 'about.html'
-        }),
-    ],
+    }
 }
